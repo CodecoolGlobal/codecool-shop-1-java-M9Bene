2e04f133998b6af3733972e6b1397b24a359075b
Kozma Gábor <kozma.jr@gmail.com>
2022-09-20 12:11:12 +0200

    Add: servlet for shopping cart

---
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -0,0 +1,8 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "shoppingCartController", urlPatterns = {"/shopping-cart"})
+public class ShoppingCartController extends HttpServlet {
+}
