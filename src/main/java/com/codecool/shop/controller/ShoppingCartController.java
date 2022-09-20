812d58a2f75ef4d348cdb46125a3705b1d01b3c8
Kozma Gábor <kozma.jr@gmail.com>
2022-09-20 13:15:29 +0200

    Add: serialize requested product

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -1,8 +1,32 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "shoppingCartController", urlPatterns = {"/shopping-cart"})
 public class ShoppingCartController extends HttpServlet {
+
+    ProductDao productDao = ProductDaoMem.getInstance();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        int productId = Integer.parseInt(request.getParameter("id"));
+        Product product = productDao.find(productId);
+
+        Gson gson = new Gson();
+        PrintWriter out = response.getWriter();
+
+        out.println(gson.toJson(product));
+    }
 }
