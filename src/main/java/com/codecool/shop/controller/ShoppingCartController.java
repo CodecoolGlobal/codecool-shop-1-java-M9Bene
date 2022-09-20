478acb962bcf4779633632a78f2c7aa5b9f1c2bd
Kozma Gábor <kozma.jr@gmail.com>
2022-09-20 13:50:46 +0200

    Add: interface and implementation for shopping cart DAO

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -1,7 +1,9 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
 
@@ -17,12 +19,16 @@ import java.io.PrintWriter;
 public class ShoppingCartController extends HttpServlet {
 
     ProductDao productDao = ProductDaoMem.getInstance();
+    ShoppingCartDao shoppingCart = ShoppingCartDaoMem.getInstance();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
         int productId = Integer.parseInt(request.getParameter("id"));
         Product product = productDao.find(productId);
+
+        shoppingCart.add(product);
 
         Gson gson = new Gson();
         PrintWriter out = response.getWriter();
