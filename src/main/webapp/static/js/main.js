dcd078fbb7e57ee0ebca157aa7d3aa34fb44ae5b
megaseves <megaseves@gmail.com>
2022-09-22 14:41:35 +0200

    Add: modal login & register

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -8,16 +8,22 @@ const addToCartButtons = document.querySelectorAll('.buttonAddToCart');
 const removeFromCartButtons = document.querySelectorAll('.buttonRemove');
 const plusQuantityButtons = document.querySelectorAll('.plus-button');
 const minusQuantityButtons = document.querySelectorAll('.minus-button');
-
 const loadButtonForCartPage = document.querySelector('#load-cart-page');
+const loadLoginRegisterIcon = document.querySelector('#load-login-register-icon');
+const modalForLoginRegister = document.querySelector('.modalForLoginRegister');
+const loginButton = document.querySelector('.loginButton');
+const modalForLogin = document.querySelector('.modalForLogin');
+const container = document.querySelector('.container');
+const modalForRegister = document.querySelector('.modalForRegister');
+const registerButton = document.querySelector('.registerButton');
 
 HomeButton.addEventListener("click", HomePageLoad);
 categoryMenuButton.addEventListener('mouseover', ModalDisplayForCategory);
-supplierMenuButton.addEventListener('mouseover', ModalDisplayForSupplier)
-
-
-
+supplierMenuButton.addEventListener('mouseover', ModalDisplayForSupplier);
 loadButtonForCartPage.addEventListener('click', addLoadForCartPage)
+loadLoginRegisterIcon.addEventListener('mouseover', ModalDisplayForLoginRegister);
+loginButton.addEventListener('click', ModalDisplayForLogin);
+registerButton.addEventListener('click', ModalDisplayForRegister);
 
 
 function HomePageLoad() {
@@ -29,10 +35,16 @@ function HideModal(modalWithListener, modalToHide) {
         modalToHide.classList.add('modalDisplayHidden');
     });
 }
+function HideModalWithClick(modalWithListener, modalToHide) {
+    modalWithListener.addEventListener('click', (event) => {
+        modalToHide.classList.add('modalDisplayHidden');
+    });
+}
 
 function ModalDisplayForCategory() {
     ModalForCategory.classList.remove('modalDisplayHidden');
     modalForSupplier.classList.add('modalDisplayHidden');
+    modalForLoginRegister.classList.add('modalDisplayHidden');
     HideModal(ModalForCategory, ModalForCategory);
     HideModal(headerMenu, ModalForCategory);
 }
@@ -40,9 +52,31 @@ function ModalDisplayForCategory() {
 function ModalDisplayForSupplier() {
     modalForSupplier.classList.remove('modalDisplayHidden');
     ModalForCategory.classList.add('modalDisplayHidden');
+    modalForLoginRegister.classList.add('modalDisplayHidden');
     HideModal(modalForSupplier, modalForSupplier);
     HideModal(headerMenu, modalForSupplier);
 }
+
+function ModalDisplayForLoginRegister() {
+    modalForLoginRegister.classList.remove('modalDisplayHidden');
+    ModalForCategory.classList.add('modalDisplayHidden');
+    modalForSupplier.classList.add('modalDisplayHidden');
+    HideModal(modalForLoginRegister, modalForLoginRegister);
+    HideModal(headerMenu, modalForLoginRegister);
+}
+
+
+function ModalDisplayForLogin() {
+    modalForLogin.classList.remove('modalDisplayHidden');
+    modalForLoginRegister.classList.add('modalDisplayHidden');
+    HideModalWithClick(container, modalForLogin);
+}
+function ModalDisplayForRegister() {
+    modalForRegister.classList.remove('modalDisplayHidden');
+    modalForLoginRegister.classList.add('modalDisplayHidden');
+    HideModalWithClick(container, modalForRegister);
+}
+
 
 function addLoadForCartPage() {
     window.location.href = '/shopping-cart';
